bcb5f956b8c2ec0901b61974ca9aa531c15b809e
itskumod <158336313+itskumod@users.noreply.github.com>
2026-08-27 10:46:37 +0000

    conversion

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,12 @@
+let score="67abc"
+
+console.log(typeof score)
+let no=Number(score)
+console.log(typeof no)
+console.log(no)
+
+//"33"=>33,"33abc"=>NaN,true=>1
+
+let islogin=1;
+let b=Boolean(islogin)
+console.log(b)
