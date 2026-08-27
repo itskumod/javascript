50df59762d4f0d5ca9c04071e0b6acdc984ed614
itskumod <158336313+itskumod@users.noreply.github.com>
2026-08-27 10:33:09 +0000

    data_types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+//alert(3+2)
+//console.log(3+3)
+//string
+let name="Radha"
+//number
+let age=21
+//boolean
+let isLogin=false
+//bigint
+//null=>standalone val
+//undefined
+//symbol=>unique
+
+//object
+
+console.table(typeof name)
+console.table(typeof age)
+console.table(typeof isLogin)
+console.table(typeof undefined)
+console.table(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accId=14452
+let accEmail="abc@gmail.com"
+var accPasswoed="12345"
+accCity="Purnea"
+let accState;
+
+//accId=2
+accEmail="pqr@gmail.com"
+accPasswoed="6789"
+accCity="Katihar"
+
+
+/*
+not use var issue in block and function scope
+ */
+
+console.log(accId);
+console.table([accId,accPasswoed,accEmail,accCity,accState])
