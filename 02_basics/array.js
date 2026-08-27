c4d453990811eea349a45d51c6f6039c658d3a2a
itskumod <158336313+itskumod@users.noreply.github.com>
2026-08-27 21:13:11 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,37 @@
+//array
+
+const myarr=[1,2,3,4,5,6]
+// console.log(myarr[3])
+
+const newarr=new Array(2,3,4,5,6,7)
+// console.log(myarr.length)
+// myarr.push(23)
+// //myarr.pop()
+// myarr.unshift(7)
+
+
+// console.log(myarr.includes(9))
+// console.log(myarr.indexOf(3))
+
+// const narr=myarr.join()
+
+// console.log(narr)
+// console.log(myarr)
+// const n1=myarr.slice(2,4)
+// console.log(myarr)
+// console.log(n1)
+
+const marvel_heros=["thor","ironman","padman"]
+const dc_heros=["superman","flash","batman"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][2])
+
+// const a=marvel_heros.concat(dc_heros)
+// console.log(a)
+// let b=[2,4,2,3,[5,7,8,6,9,[2,3,1]]]
+// const c=b.flat(Infinity)
+// console.log(c)
+
+console.log(Array.isArray("kumod"))
+console.log(Array.from("kumod"))
