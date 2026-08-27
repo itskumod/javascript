39bcf8461df61fa27e1659698b5944e15c196ef5
itskumod <158336313+itskumod@users.noreply.github.com>
2026-08-27 11:30:10 +0000

    datatype summery

---
+++ 01_basics/dtsumm.js
@@ -0,0 +1,35 @@
+//primitive
+//7 type->String ,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outside_temp=null
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+const bigint=37846648625436845;
+
+//console.log(id===anotherId)
+
+//refrence(non premative)
+//Array,Object,Function
+
+const heros=["batman","Iron man","Spider man","Pad man"]
+
+
+let myobj={
+    name:"subhash",
+    age: 21,
+    gen: "male",
+}
+
+const myfun=function(){
+    console.log("hello world")
+
+}
+
+console.log(typeof bigint)
