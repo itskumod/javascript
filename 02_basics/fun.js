152ea9ff6dbee67b4c612785716204a839da05d7
itskumod <158336313+itskumod@users.noreply.github.com>
2026-08-27 22:53:23 +0000

    function

---
+++ 02_basics/fun.js
@@ -0,0 +1,25 @@
+// function sayname(){
+//     console.log("K")
+//     console.log("u")
+
+//     console.log("m")
+
+//     console.log("o")
+
+//     console.log("d")
+
+
+// }
+
+// sayname()
+
+// function add(num1,num2){
+//     return num1+num2
+// }
+// console.log(add(3,2))
+
+function calculateCartPrice(...num1){
+    return num1;
+}
+
+console.log(calculateCartPrice(200,400,900))
