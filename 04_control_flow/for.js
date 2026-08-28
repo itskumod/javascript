c39b359dbddf178a6a4b42bd250a103da02c5cbd
itskumod <158336313+itskumod@users.noreply.github.com>
2026-08-28 18:13:35 +0000

    flow control

---
+++ 04_control_flow/for.js
@@ -0,0 +1,22 @@
+//for
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// //break continue
+
+// //Nested for loop
+// let i=10
+
+// while(i--){
+//     console.log(i);
+// }
+
+let score=11;
+
+// do{
+//    console.log(`score is ${score}`)
+//    score++
+// }while(score<=10)
+
---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+// //if
+
+// //condition- >,<,<=,>=.!=,===,||,&&,!==
+
+// let temp=45
+
+// if(temp>30){
+
+//     console.log()
+
+// }
+// else{
+
+// }
+
+// if(){
+
+// }
+// else if(){
+
+// }
+// else{
+
+// }
+
+// let day="sunday"
+// switch (day){
+//     case "sunday":
+//         console.log("Going for movie")
+//         break
+//     case "tuesday":
+//         console.log("going for dinner")
+//         break
+
+//     case "wednesday":
+//         console.log("goning for football")
+//         break
+
+// }
+
+//false,0,-0,Bigint 0n,"",null,undefined,NaN
+
+// val1= null??34
+// console.log(val1)
+
+
+//ternary operator
+
+const icecream=200
+icecream<=80?console.log("less than 80"):console.log("more than 80")
