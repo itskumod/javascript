8c224592b48b388aac2a3500dade6eeedbca9173
itskumod <158336313+itskumod@users.noreply.github.com>
2026-08-28 20:59:25 +0000

    foreach map filter

---
+++ 04_control_flow/three.js
@@ -0,0 +1,43 @@
+//for of
+
+// const arr=[1,2,3,5,4]
+
+// for(const num of arr){
+//     console.log(num)
+// }
+
+//Map
+// const map=new Map()
+
+// map.set('IN',"India")
+// map.set('usa',"America")
+// map.set('un',"united state")
+// map.set('pak',"pakistan")
+// map.set('ban',"Banladesh")
+
+// console.log (map)
+
+ let arr=[23,45,22,44,67,32,57]
+// arr.forEach(function(val){
+//     console.log(val)
+// })
+
+// arr.forEach((val)=>{
+//     console.log(val)
+// })
+
+// const coding=["js","ruby","java","python","cpp"]
+
+// const values=coding.forEach((item)=>{
+//     return item
+// })
+// console.log(values)
+
+//let fl=arr.filter((num)=>num>30)
+
+// let fl=arr.filter((num)=>{return num>30})
+
+// console.log(fl)
+
+const newnum=arr.map((num)=>num+10)
+console.log(newnum)
