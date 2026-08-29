f4c3668638482bf9b2d84efcd9915732fa117d4b
itskumod <158336313+itskumod@users.noreply.github.com>
2026-08-29 10:18:28 +0000

    project one

---
+++ Events/addEventListener.js
@@ -0,0 +1,22 @@
+//event listener
+const buttons=document.querySelectorAll('.button')
+const body=document.querySelector('body')
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',function(e){
+    if(e.target.id=="grey"){
+        body.style.backgroundColor="grey"
+    }
+    if(e.target.id=="white"){
+        body.style.backgroundColor="white"
+    }
+    if(e.target.id=="blue"){
+        body.style.backgroundColor="blue"
+    }
+    if(e.target.id=="purple"){
+        body.style.backgroundColor="purple"
+    }   
+
+})
+
+})
